b4b629c7e62fe4d09ce4b0e4cae29e7428a0869d
João Barradas <joao@jbarradas.com>
2024-03-05 17:47:09 +0000

    Livereload no ssl

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ let online = 0;
 let leadClient;
 
 // Live Reload for client /public files
-const liveReloadServer = livereload.createServer({
-  https: {
-    key: fs.readFileSync(path.join(__dirname, "./certs/websockets_bounce.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "./certs/cert.pem")),
-  },
-});
+const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, "public"));
 
 liveReloadServer.server.once("connection", () => {
